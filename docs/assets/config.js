ad39040887828b9096030d7b562179552954e51c
co2xsolution <co2xsolution@gmail.com>
2026-09-10 09:41:00 +0800

    Update config.js

--- docs/assets/config.js
+++ docs/assets/config.js
@@ -1,6 +1,6 @@
 /* Public configuration. Never put passwords, access tokens or API secrets here. */
 window.LIMS_CONFIG = Object.freeze({
-  apiUrl: '', // Paste https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec
+  apiUrl: 'https://script.google.com/macros/s/AKfycbz1SY5OwtHPcBL6Yt8zVP0DN3PipdgJ76tcXa6hYnEPj6tnmw4EYBouA09tVCqtIQ8/exec', // Paste https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec
   labName: 'Laboratory',
   timeoutMs: 90000
 });
